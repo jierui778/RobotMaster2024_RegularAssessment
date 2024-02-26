6047da8713dfe7e12521cbeaf491ef00619247ff
jierui778 <758418101@qq.com>
2024-02-27 00:08:14 +0800

    本次提交为：增加Motor.c部分函数注释

--- SourceCode/RegularAssessment/Core/Src/Motor.c
+++ SourceCode/RegularAssessment/Core/Src/Motor.c
@@ -12,25 +12,38 @@
 #include "can.h"
 #include "main.h"
 
-// motor_info_t motor_info[2]; // 测试双电机
-
+motor_info_t motor_info[2]; // 测试双电机
+/**
+ * @brief 初始化电机CAN配置
+ *
+ */
 void Motor_Can_Init(void)
 {
     // Motor_Init
 }
+/**
+ * @brief 初始化电机发送配置
+ *
+ */
 void Motor_TXInfo_Init(void)
 {
     // Motor_TXInfo_Init
     for (uint8_t i = 0; i < 3; i++)
     {
-        
     }
 }
-
-void Send_GimbalInfo(void)
+/**
+ * @brief can发送电机信息
+ *
+ */
+void Send_MotorInfo(void)
 {
     // Send_GimbalInfo
 }
+/**
+ * @brief can发送电机信息
+ *
+ */
 void Receive_MotorInfo(void)
 {
     // Receive_MotorInfo
